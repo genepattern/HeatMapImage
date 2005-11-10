1476fb63664cbf0095fa43ba4e0ce59559341740
jgould <jgould@47fccdf2-e235-0410-87e5-63258e95d6e5>
2005-11-10 20:11:32 +0000

    moved to common
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/HeatMapImage@7538 47fccdf2-e235-0410-87e5-63258e95d6e5

--- src/edu/mit/broad/modules/heatmap/ClassVectorListener.java
+++
@@ -1,7 +0,0 @@
-package edu.mit.broad.modules.heatmap;
-
-interface ClassVectorListener extends java.util.EventListener {
-	
-	void classVectorChanged(java.util.EventObject e);
-
-}
--- src/edu/mit/broad/modules/heatmap/ElementSizeChangedListener.java
+++
@@ -1,6 +0,0 @@
-package edu.mit.broad.modules.heatmap;
-interface ElementSizeChangedListener extends java.util.EventListener {
-	
-	void elementSizeChanged(ElementSizeChangedEvent e);
-
-}
